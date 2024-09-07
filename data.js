1aade56e1a80a3c365f2b9fae74122020243ffeb
sellylima <vanessa.limanasc@gmail.com>
2024-09-07 16:32:44 -0300

    style css

--- data.js
+++ data.js
@@ -1,51 +1,107 @@
 let dados = [
-
-    {
-        titulo : " Rafaella Ballerini",
-        descricao : "Mais presente no Instagram, Rafaella compartilha conteúdos sobre HTML e CSS, além de dar opiniões sobre carreira, cursos e qual área da tecnologia seguir.",
-        link : "https://www.youtube.com/@rafaellaballerini",
-        tags: "front-end, frontend, front end"
-    },
-    {
-        titulo: "Gabriela Vaz (Gabcodes)",
-        descricao: "Desenvolvedora front-end com grande influência nas redes sociais. Compartilha seus conhecimentos sobre HTML, CSS, JavaScript e React.",
-        link: "https://www.instagram.com/gabcodes/" ,
-        tags: "frontend instagram"
-    },
-    {
-        titulo: "Glaucia Lemos",
-        descricao: "Desenvolvedora Cloud Advocate em JavaScript / Node.js e TypeScript na Microsoft. Além disso, realiza contribuições para projetos de código aberto no GitHub. Glaucia também pertence à WoMakersCode, uma organização focada em capacitar mulheres na tecnologia.",
-        link: "https://www.youtube.com/c/GlauciaLemos",
-        tags: "frontend professora youtube"
-    },
-    {
-        titulo: "Simara Conceição",
-        descricao: "Desenvolvedora front-end e criadora de conteúdo no YouTube. Compartilha tutoriais e dicas de programação, com foco em JavaScript e React.",
-        link: "https://www.youtube.com/@SimaraConceicao",
-        tags: "frontend, front end, dica youtube"
-      },
-      {
-        titulo: "Loiane Groner",
-        descricao: "Desenvolvedora e instrutora, especialista em Java e Angular, autora de livros e cursos online. Compartilha conteúdos sobre programação e desenvolvimento front-end.",
-        link: "https://loiane.com",
-        tags: "framework, framework angular, site youtube backend back-end "
-      },
-      {
-        titulo: "Vanessa Tonini",
-        descricao: "Designer de UX e desenvolvedora, com foco em acessibilidade e usabilidade. Compartilha conteúdos sobre design e desenvolvimento web.",
-        link: "https://www.linkedin.com/in/vanessatonini",
-        tags: "ux designer design linkedin"
-      },
-      {
-        titulo: "Ana Cecília Vieira",
-        descricao: "Desenvolvedora front-end e streamer na Twitch. Foca em acessibilidade, performance e desenvolvimento web, com projetos ao vivo.",
-        link: "https://www.twitch.tv/anaceciliavieira",
-        tags: "back-end backend twitch"
-      },
-      {
-        titulo: "Lilit Bandeira",
-        descricao: "Engenheira de software e desenvolvedora front-end, defensora da inclusão de mulheres e pessoas LGBTQIA+ na tecnologia.",
-        link: "https://www.linkedin.com/in/lilitbandeira",
-        tags: "engenheira linkedin"
-      }
-];
+  {
+    titulo: 'Rafaella Ballerini',
+    descricao:
+      'Mais presente no Instagram, Rafaella compartilha conteúdos sobre HTML e CSS, além de dar opiniões sobre carreira, cursos e qual área da tecnologia seguir.',
+    link: 'https://www.youtube.com/@rafaellaballerini',
+    tags: 'HTML, CSS, front-end, frontend, carreira, cursos, tecnologia, desenvolvedora',
+  },
+  {
+    titulo: 'Gabriela Vaz (Gabcodes)',
+    descricao:
+      'Desenvolvedora front-end com grande influência nas redes sociais. Compartilha seus conhecimentos sobre HTML, CSS, JavaScript e React.',
+    link: 'https://www.instagram.com/gabcodes/',
+    tags: 'frontend, frontend, denvolvedora, HTML, CSS, JavaScript, React, Instagram',
+  },
+  {
+    titulo: 'Glaucia Lemos',
+    descricao:
+      'Desenvolvedora Cloud Advocate em JavaScript / Node.js e TypeScript na Microsoft. Além disso, realiza contribuições para projetos de código aberto no GitHub. Glaucia também pertence à WoMakersCode, uma organização focada em capacitar mulheres na tecnologia.',
+    link: 'https://www.youtube.com/c/GlauciaLemos',
+    tags: 'Cloud Advocate, JavaScript, Node.js, TypeScript, GitHub, WoMakersCode, YouTube',
+  },
+  {
+    titulo: 'Simara Conceição',
+    descricao:
+      'Desenvolvedora front-end e criadora de conteúdo no YouTube. Compartilha tutoriais e dicas de programação, com foco em JavaScript e React.',
+    link: 'https://www.youtube.com/@SimaraConceicao',
+    tags: 'front-end, frontend, JavaScript, React, tutoriais, Desenvolvedora front-end, YouTube',
+  },
+  {
+    titulo: 'Loiane Groner',
+    descricao:
+      'Desenvolvedora e instrutora, especialista em Java e Angular, autora de livros e cursos online. Compartilha conteúdos sobre programação e desenvolvimento front-end.',
+    link: 'https://loiane.com',
+    tags: 'Java, Angular, programação, cursos online, livros, site, backend, back-end, back end',
+  },
+  {
+    titulo: 'Vanessa Tonini',
+    descricao:
+      'Designer de UX e desenvolvedora, com foco em acessibilidade e usabilidade. Compartilha conteúdos sobre design e desenvolvimento web.',
+    link: 'https://www.linkedin.com/in/vanessatonini',
+    tags: 'UX, design, acessibilidade, usabilidade, LinkedIn',
+  },
+  {
+    titulo: 'Ana Cecília Vieira',
+    descricao:
+      'Desenvolvedora front-end e streamer na Twitch. Foca em acessibilidade, performance e desenvolvimento web, com projetos ao vivo.',
+    link: 'https://www.twitch.tv/anaceciliavieira',
+    tags: 'front-end, acessibilidade, performance, desenvolvimento web, Twitch',
+  },
+  {
+    titulo: 'Lilit Bandeira',
+    descricao:
+      'Engenheira de software e desenvolvedora front-end, defensora da inclusão de mulheres e pessoas LGBTQIA+ na tecnologia.',
+    link: 'https://www.linkedin.com/in/lilitbandeira',
+    tags: 'engenheira de software, front-end, inclusão, LinkedIn',
+  },
+  {
+    titulo: 'Bruna Andrade',
+    descricao:
+      "Desenvolvedora de software e fundadora da 'Tech Women Brasil'. Ela é bastante ativa no Twitter e no LinkedIn, compartilhando dicas e novidades sobre tecnologia e diversidade.",
+    link: 'https://twitter.com/BrunaAndrade',
+    tags: 'desenvolvedora de software, diversidade, tecnologia',
+  },
+  {
+    titulo: 'Silvia Rodrigues',
+    descricao:
+      'Engenheira de software e influenciadora digital. Ela posta sobre programação e desenvolvimento no Instagram e no LinkedIn.',
+    link: 'https://www.instagram.com/silvia.rodrigues',
+    tags: 'engenheira de software, programação, desenvolvimento',
+  },
+  {
+    titulo: 'Letícia Costa',
+    descricao:
+      'Especialista em cibersegurança e cofundadora do CyberSec Mulheres. Ela é ativa no Twitter e no LinkedIn, onde compartilha informações sobre segurança digital e diversidade na tecnologia.',
+    link: 'https://twitter.com/LeticiaCosta',
+    tags: 'cibersegurança, segurança digital, diversidade',
+  },
+  {
+    titulo: 'Camila Achutti',
+    descricao:
+      "Fundadora da 'MasterTech' e uma conhecida palestrante e influenciadora na área de tecnologia. Ela é muito ativa no LinkedIn e Instagram, onde compartilha conteúdo sobre desenvolvimento e empoderamento feminino.",
+    link: 'https://www.linkedin.com/in/camila-achutti',
+    tags: 'fundadora, MasterTech, desenvolvimento, empoderamento feminino',
+  },
+  {
+    titulo: 'Luana Almeida',
+    descricao:
+      'Desenvolvedora de software e criadora de conteúdo digital. Ela é ativa no YouTube e no Twitter, onde compartilha tutoriais e dicas sobre programação.',
+    link: 'https://www.youtube.com/@LuanaAlmeida',
+    tags: 'desenvolvedora de software, tutoriais, programação',
+  },
+  {
+    titulo: 'Daniella Figueiredo',
+    descricao:
+      "Designer UX/UI e cofundadora da 'Women Who Code Brasil'. Ela é ativa no LinkedIn e Instagram, abordando temas sobre design e inclusão na tecnologia.",
+    link: 'https://www.linkedin.com/in/daniella-figueiredo',
+    tags: 'designer UX/UI, Women Who Code, inclusão, design',
+  },
+  {
+    titulo: 'Giselle Bezerra',
+    descricao:
+      'Cientista de dados e professora. Ela compartilha suas experiências e insights no LinkedIn e Twitter, com foco em dados e aprendizado de máquina.',
+    link: 'https://www.linkedin.com/in/giselle-bezerra',
+    tags: 'cientista de dados, aprendizado de máquina, dados',
+  },
+]
