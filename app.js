669c4c064ba65b14131a4a1637384d778cfebfb8
sellylima <vanessa.limanasc@gmail.com>
2024-09-07 01:15:21 -0300

    add function pesquisar

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
-function search() {
-    // Obtém a seção onde os resultados serão exibidos
-    let section = document.getElementById('resultados-pesquisa');
-  
-    // Inicializa uma string vazia para armazenar o HTML dos resultados
-    let result = '';
-  
-    // Itera sobre os dados de cada mulher na tecnologia e cria o HTML dos resultados
-    for (let data of dataMulheresTech) {
-      result += `
-        <div class="item-resultado">
-          <h2>${data.titulo}</h2>
-          <p class="descricao-meta">${data.descricao}</p>
-          <a href="${data.link}" target="_blank">Mais informações</a>
-        </div>
-      `;
-    }
-  
-    // Atualiza o conteúdo da seção com os resultados gerados
-    section.innerHTML = result;
+function pesquisar() {
+  // Obtém a seção onde os resultados serão exibidos
+  let section = document.getElementById('resultados-pesquisa')
+  let campoPesquisa = document.getElementById('campo-pesquisa').value
+
+  if (campoPesquisa == "") {
+    section.innerHTML = "<p>Nenhum resultado encontrado</p>"
+    return
   }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  // Inicializa uma string vazia para armazenar o HTML dos resultados
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  // Itera sobre os dados de cada mulher na tecnologia e cria o HTML dos resultados
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // Cria um novo elemento HTML para cada resultado
+      resultados += `
+      <div class="item-resultado">
+        <h2>${dado.titulo}</h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href="${dado.link}" target="_blank">Mais informações</a>
+      </div>
+`;
+    }
+  }
+
+  if (!resultados) {
+    resultados = "<p>Nenhum resultado encontrado</p>"
+  }
+  // Atualiza o conteúdo da seção com os resultados gerados
+  section.innerHTML = resultados
+}
--- data.js
+++ data.js
@@ -1,43 +1,51 @@
-let dataMulheresTech = [
+let dados = [
 
     {
         titulo : " Rafaella Ballerini",
         descricao : "Mais presente no Instagram, Rafaella compartilha conteúdos sobre HTML e CSS, além de dar opiniões sobre carreira, cursos e qual área da tecnologia seguir.",
-        link : "https://www.youtube.com/@rafaellaballerini"
+        link : "https://www.youtube.com/@rafaellaballerini",
+        tags: "front-end frontend professora youtube influencer instagram"
     },
     {
         titulo: "Gabriela Vaz (Gabcodes)",
         descricao: "Desenvolvedora front-end com grande influência nas redes sociais. Compartilha seus conhecimentos sobre HTML, CSS, JavaScript e React.",
-        link: "https://www.instagram.com/gabcodes/" 
+        link: "https://www.instagram.com/gabcodes/" ,
+        tags: "frontend instagram"
     },
     {
         titulo: "Glaucia Lemos",
         descricao: "Desenvolvedora Cloud Advocate em JavaScript / Node.js e TypeScript na Microsoft. Além disso, realiza contribuições para projetos de código aberto no GitHub. Glaucia também pertence à WoMakersCode, uma organização focada em capacitar mulheres na tecnologia.",
-        link: "https://www.youtube.com/c/GlauciaLemos" 
+        link: "https://www.youtube.com/c/GlauciaLemos",
+        tags: "frontend professora youtube"
     },
     {
         titulo: "Simara Conceição",
         descricao: "Desenvolvedora front-end e criadora de conteúdo no YouTube. Compartilha tutoriais e dicas de programação, com foco em JavaScript e React.",
-        link: "https://www.youtube.com/@SimaraConceicao"
+        link: "https://www.youtube.com/@SimaraConceicao",
+        tags: "frontend, front end, dica youtube"
       },
       {
         titulo: "Loiane Groner",
         descricao: "Desenvolvedora e instrutora, especialista em Java e Angular, autora de livros e cursos online. Compartilha conteúdos sobre programação e desenvolvimento front-end.",
-        link: "https://loiane.com"
+        link: "https://loiane.com",
+        tags: "framework, framework angular, site youtube backend back-end "
       },
       {
         titulo: "Vanessa Tonini",
         descricao: "Designer de UX e desenvolvedora, com foco em acessibilidade e usabilidade. Compartilha conteúdos sobre design e desenvolvimento web.",
-        link: "https://www.linkedin.com/in/vanessatonini"
+        link: "https://www.linkedin.com/in/vanessatonini",
+        tags: "ux designer design linkedin"
       },
       {
         titulo: "Ana Cecília Vieira",
         descricao: "Desenvolvedora front-end e streamer na Twitch. Foca em acessibilidade, performance e desenvolvimento web, com projetos ao vivo.",
-        link: "https://www.twitch.tv/anaceciliavieira"
+        link: "https://www.twitch.tv/anaceciliavieira",
+        tags: "back-end backend twitch"
       },
       {
         titulo: "Lilit Bandeira",
         descricao: "Engenheira de software e desenvolvedora front-end, defensora da inclusão de mulheres e pessoas LGBTQIA+ na tecnologia.",
-        link: "https://www.linkedin.com/in/lilitbandeira"
+        link: "https://www.linkedin.com/in/lilitbandeira",
+        tags: "engenheira linkedin"
       }
 ];
