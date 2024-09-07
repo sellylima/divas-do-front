4683c3bf38364f8f464eea32d811fbe813115d2f
sellylima <vanessa.limanasc@gmail.com>
2024-09-07 19:10:49 -0300

    ultimos ajustes

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
 function pesquisar() {
-  // Obtém a seção onde os resultados serão exibidos
-  let section = document.getElementById('resultados-pesquisa')
-  let campoPesquisa = document.getElementById('campo-pesquisa').value
+  // Obtém a seção onde os resultados serão exibidos e o valor do campo de pesquisa
+  let section = document.getElementById('resultados-pesquisa');
+  let campoPesquisa = document.getElementById('campo-pesquisa').value.toLowerCase();
 
-  if (campoPesquisa == "") {
-    section.innerHTML = "<p>Nenhum resultado encontrado</p>"
-    return
+  // Verifica se o campo de pesquisa está vazio
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Nenhum resultado encontrado</p>";
+    return; // Interrompe a função se o campo estiver vazio
   }
 
-  campoPesquisa = campoPesquisa.toLowerCase();
-
-  // Inicializa uma string vazia para armazenar o HTML dos resultados
+  // Inicializa variáveis para armazenar os resultados e os dados de cada elemento
   let resultados = "";
-  let titulo = "";
-  let descricao = "";
-  let tags = "";
+  let titulo, descricao, tags;
 
-  // Itera sobre os dados de cada mulher na tecnologia e cria o HTML dos resultados
+  // Itera sobre os dados, compara com a pesquisa e cria o HTML dos resultados
   for (let dado of dados) {
-    titulo = dado.titulo.toLowerCase()
-    descricao = dado.descricao.toLowerCase()
-    tags = dado.tags.toLowerCase()
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+
+    // Verifica se a pesquisa existe no título, descrição ou tags
     if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
-      // Cria um novo elemento HTML para cada resultado
+      // Cria um novo elemento HTML para cada resultado que coincida com a pesquisa
       resultados += `
-      <div class="item-resultado">
-        <h2>${dado.titulo}</h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href="${dado.link}" target="_blank">Rede Social</a>
-      </div>
-`;
+        <div class="item-resultado">
+          <h2>${dado.titulo}</h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href="${dado.link}" target="_blank">Rede Social</a>
+        </div>
+      `;
     }
   }
 
-  if (!resultados) {
-    resultados = "<p>Nenhum resultado encontrado</p>"
-  }
-  // Atualiza o conteúdo da seção com os resultados gerados
-  section.innerHTML = resultados
+  // Verifica se foram encontrados resultados e atualiza a seção
+  section.innerHTML = resultados || "<p>Nenhum resultado encontrado</p>";
 }
