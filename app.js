061cd0f64aeeb1bfa8ee03ce1f5fd7a54aad34ec
sellylima <vanessa.limanasc@gmail.com>
2024-09-06 22:38:12 -0300

    add search functionality

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(data);
+function search() {
+  let section = document.getElementById('resultados-pesquisa')
+
+  let result = ''
+
+  // Loop de Repetição
+  for (let data of dataMulheresTech) {
+    result += `
+            <div class="item-resultado">
+                <h2>${data.titulo}</h2>
+                <p class="descricao-meta">${data.descricao}</p>
+                <a href="${data.link}" target="_blank">Mais informações</a>
+            </div>
+        `
+  }
+
+  section.innerHTML = result
+}
