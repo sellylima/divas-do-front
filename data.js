93a318f62ba271aabb74968638e7ef793649b55e
sellylima <vanessa.limanasc@gmail.com>
2024-09-06 21:28:43 -0300

    data implementation

---
+++ data.js
@@ -0,0 +1,43 @@
+let dataMulheresTech = [
+
+    {
+        titulo : " Rafaella Ballerini",
+        descricao : "Mais presente no Instagram, Rafaella compartilha conteúdos sobre HTML e CSS, além de dar opiniões sobre carreira, cursos e qual área da tecnologia seguir.",
+        link : "https://www.youtube.com/@rafaellaballerini"
+    },
+    {
+        titulo: "Gabriela Vaz (Gabcodes)",
+        descricao: "Desenvolvedora front-end com grande influência nas redes sociais. Compartilha seus conhecimentos sobre HTML, CSS, JavaScript e React.",
+        link: "https://www.instagram.com/gabcodes/" 
+    },
+    {
+        titulo: "Glaucia Lemos",
+        descricao: "Desenvolvedora Cloud Advocate em JavaScript / Node.js e TypeScript na Microsoft. Além disso, realiza contribuições para projetos de código aberto no GitHub. Glaucia também pertence à WoMakersCode, uma organização focada em capacitar mulheres na tecnologia.",
+        link: "https://www.youtube.com/c/GlauciaLemos" 
+    },
+    {
+        titulo: "Simara Conceição",
+        descricao: "Desenvolvedora front-end e criadora de conteúdo no YouTube. Compartilha tutoriais e dicas de programação, com foco em JavaScript e React.",
+        link: "https://www.youtube.com/@SimaraConceicao"
+      },
+      {
+        titulo: "Loiane Groner",
+        descricao: "Desenvolvedora e instrutora, especialista em Java e Angular, autora de livros e cursos online. Compartilha conteúdos sobre programação e desenvolvimento front-end.",
+        link: "https://loiane.com"
+      },
+      {
+        titulo: "Vanessa Tonini",
+        descricao: "Designer de UX e desenvolvedora, com foco em acessibilidade e usabilidade. Compartilha conteúdos sobre design e desenvolvimento web.",
+        link: "https://www.linkedin.com/in/vanessatonini"
+      },
+      {
+        titulo: "Ana Cecília Vieira",
+        descricao: "Desenvolvedora front-end e streamer na Twitch. Foca em acessibilidade, performance e desenvolvimento web, com projetos ao vivo.",
+        link: "https://www.twitch.tv/anaceciliavieira"
+      },
+      {
+        titulo: "Lilit Bandeira",
+        descricao: "Engenheira de software e desenvolvedora front-end, defensora da inclusão de mulheres e pessoas LGBTQIA+ na tecnologia.",
+        link: "https://www.linkedin.com/in/lilitbandeira"
+      }
+];
